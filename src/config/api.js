ae13a46685e2a234678714631512deac3ba87fe2
Adrián <adri4silva@gmail.com>
2019-04-06 18:04:30 +0200

    Hide keys

--- src/config/api.js
+++ src/config/api.js
@@ -1,3 +1,3 @@
 export const BASE_URL = 'https://gateway.marvel.com/v1/public';
-export const AUTH_TOKEN = '0a573168635744076acdb0986e968c84';
-export const AUTH_PRIVATE = 'c41e4d830e049853df2c8331ecc6dbd01c6d61ae';
+export const AUTH_TOKEN = '###ENTER YOUR PUBLIC KEY###';
+export const AUTH_PRIVATE = '###ENTER YOUR PRIVATE KEY###';
